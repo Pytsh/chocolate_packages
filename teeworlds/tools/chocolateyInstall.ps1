7cb917b1676b0cbab71b56d089cba96b9789fe84
Pytsh <hadrien.didelot@gmail.com>
2013-07-22 11:16:51 +0200

    Create chocolateyInstall.ps1

---
+++ teeworlds/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+$packageName = 'teeworlds'
+$url = 'http://www.teeworlds.com/files/teeworlds-0.6.2-win32.zip'
+$url64bit = 'http://www.teeworlds.com/files/teeworlds-0.6.2-win64.zip'
+$unzipLocation = 'lib/teeworlds'
+
+try {
+  Install-ChocolateyZipPackage $packageName $url $unzipLocation $url64bit
+
+  #Write-ChocolateySuccess $packageName
+
+} catch {
+  Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+  throw
+}
